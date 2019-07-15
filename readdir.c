554f4b1dc36bfa1ccff632915e1b06e8dad11a17
BossOpen <52810368+BossOpen@users.noreply.github.com>
2019-07-15 10:03:19 +0800

    Update readdir.c
    添加后缀名匹配

--- readdir.c
+++ readdir.c
@@ -4,13 +4,14 @@
 #include <dirent.h>
 #include <sys/stat.h>
 
-#define MAX_PATH_LEN (256)
+#define MAX_PATH_LEN (256)   //最大路径长度
 
 static void trave_dir(char* path) {
     DIR *d = NULL;
     struct dirent *dp = NULL; /* readdir函数的返回值就存放在这个结构体中 */
     struct stat st;    
     char p[MAX_PATH_LEN] = {0};
+    const char ch = '.';
     
     if(stat(path, &st) < 0 || !S_ISDIR(st.st_mode)) {
         printf("invalid path: %s\n", path);
@@ -27,11 +28,17 @@ static void trave_dir(char* path) {
         if((!strncmp(dp->d_name, ".", 1)) || (!strncmp(dp->d_name, "..", 2)))
             continue;
 
+        //组成绝对路径
         snprintf(p, sizeof(p) - 1, "%s/%s", path, dp->d_name);
         stat(p, &st);
-        if(!S_ISDIR(st.st_mode)) {
-            printf("%s%s\n", p,dp->d_name);
-        } else {
+
+        if(!S_ISDIR(st.st_mode)) {  //文件
+            char *ret = strrchr(p, ch);//获取最后一个'.'的位置
+	        if(ret == NULL)
+                continue;
+            if(strcmp(ret,".mp3")==0 || strcmp(ret,".MP3")==0) //匹配后缀名
+            	printf("%s\n", p);
+        } else {     //路径
             printf("%s/\n", dp->d_name);
             trave_dir(p);
         }
@@ -58,4 +65,3 @@ int main(int argc, char **argv)
 
     return 0;
 }
-
