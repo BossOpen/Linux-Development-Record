8383c4042ac0d9cc371ebf67587675418c7863f7
BossOpen <52810368+BossOpen@users.noreply.github.com>
2019-08-27 15:03:17 +0800

    Create main.cpp

---
+++ GstPlayer/main.cpp
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <iostream>
+#include "usbhotplug/usbhotplug.h"
+#include "player/player.h"
+
+using namespace std;
+
+
+class  playerInterface :public AbstractPlayerInterface{
+public:
+    virtual void onCurrentPosition(char* position,char* duration) override {
+        std::cout<<position<<duration<<std::endl;
+    }
+    virtual void onMessage(char* msg) override {
+        std::cout<<msg<<std::endl;
+    }
+    virtual void onInitFinish() override {
+        std::cout<<"onInitFinish"<<std::endl;
+    }
+    virtual void onQuitFinish() override {
+        std::cout<<"onQuitFinish"<<std::endl;
+    }
+};
+
+int main() {
+	m_pAbstractPlayer->start();
+    while (1) {
+        usleep(500000);
+    }
+
+    return 0;
+}
